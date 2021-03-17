449809c83c6402bbfab063e9c92d5936c24a9e5f
mhantak <maggiehantak@gmail.com>
2021-03-17 12:40:23 -0400

    Updated misclassification script

--- find_misclassifications.py
+++ find_misclassifications.py
@@ -2,21 +2,24 @@
 from Salamander_lightning_model import SalamanderModel
 from salamander_data import getAllImagesDataset
 import sys
+import torch
 
-
-if len(sys.argv) != 2):
+if len(sys.argv) != 2:
     exit('ERROR: Please provide the path of a model checkpoint.')
 
 all_images = getAllImagesDataset(
     '/blue/guralnick/mhantak/categories_binary/'
 )
 
-model = SalamanderModel()
-model.load_from_checkpoint(sys.argv[1])
+model = SalamanderModel(lr=0.001)
+model.load_from_checkpoint(sys.argv[1], lr=0.001)
 
 i = 0
 for img, label in all_images:
-    outputs = model([img])
-    print(outputs, all_images.samples[i])
+    print(label)
+    outputs = model(torch.unsqueeze(img, 0))
+    #outputs = model(torch.stack([img]))
+    p_labels = torch.max(outputs, 1).indices
+    print(p_labels, all_images.samples[i])
     i += 1
 
